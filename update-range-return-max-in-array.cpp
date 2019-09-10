500f34101ce6082877bcde5507e74e06a7a3cace
A M Shahed Hasan <amsh.himu@gmail.com>
2019-09-10 14:48:00 +0600

    Create update-range-return-max-in-array.cpp

---
+++ update-range-return-max-in-array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define SZ 10000002
+using namespace std;
+
+long int arr[SZ];
+
+int main(){
+    long int n, m, a, b, k, x=0, mx=0;
+    cin>>n>>m;
+    while(m--){
+        cin>>a>>b>>k;
+        arr[a] += k;
+        if (arr[b+1] <= n)    arr[b+1] -=k;
+    }
+    for (int i=0; i<=n; i++){
+        x += arr[i];
+        if (mx<x)   mx=x;
+    }
+    cout<<mx;
+    return 0;
+}
